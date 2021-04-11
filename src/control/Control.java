b0e8194d2c149846e98054f5e89802d2e64a37a0
DanielaAgudelo13 <daniela13agudelo@hotmail.com>
2021-04-10 23:43:18 -0500

    se agrego el txt

--- src/control/Control.java
+++ src/control/Control.java
@@ -1,5 +1,17 @@
 package control;
 
+import java.util.ArrayList;
+
+import modelo.Figura;
+
 public class Control {
+	
+	ArrayList figuras = new ArrayList <Figura> (); 
+	
+	String[] data;
+	
+	public Control () {
+		
+	}
 
 }
--- src/modelo/Palabra.java
+++ src/modelo/Palabra.java
@@ -1,5 +1,18 @@
 package modelo;
 
 public class Palabra {
+	
+	String texto;   
+	
+	public Palabra (String texto) {
+		
+	this.texto = texto;
+	
+	}
+	
+	public void createObject () {
+		
+	}
 
+	
 }
